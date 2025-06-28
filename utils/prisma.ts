bf57a0cbd98b3229ea6f61a988228dc75bd83440
Domains18 <gibsonsgibson88@gmail.com>
2025-06-28 20:55:32 +0300

    feat: add Prisma schema and integrate Prisma client with Accelerate extension

---
+++ utils/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@/lib/generated/prisma'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient().$extends(withAccelerate())
+
+
+export default prisma
