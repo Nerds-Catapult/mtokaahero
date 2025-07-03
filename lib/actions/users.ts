e07cdbccb4f6fbb54cce313bbf37d391987c39f1
Domains18 <gibsonsgibson88@gmail.com>
2025-07-03 14:44:31 +0300

    refactor: move 'use server' directive to the top of users and error handler files

--- lib/actions/users.ts
+++ lib/actions/users.ts
@@ -1,3 +1,5 @@
+'use server';
+
 import prisma from "@/utils/prisma";
 import { ErrorHandlerService } from "@/middleware/erroHandler";
 
--- middleware/erroHandler.ts
+++ middleware/erroHandler.ts
@@ -1,6 +1,6 @@
+'use server';
+
 import { Prisma, PrismaClient } from "@/lib/generated/prisma";
-
-
 
 
 export class ErrorHandlerService {
